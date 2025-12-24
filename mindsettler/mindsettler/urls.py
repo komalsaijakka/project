9b7dc0ce2c9a58ee9325d7e22952aaffd4801f8e
komal <jksai2006@gmail.com>
2025-12-24 16:16:44 +0530

    w

---
+++ mindsettler/mindsettler/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import home, how_it_works, what_makes_us_different, chatbot_reply
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path("how-it-works/", how_it_works, name="how_it_works"),
+    path("what-makes-us-different/", what_makes_us_different, name="what_makes_us_different"),
+    path("chatbot-reply/", chatbot_reply, name="chatbot_reply"),
+]
